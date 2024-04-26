af7c68b7874b4e5f71b5585ca17fa8f0b226ba58
Jaime Prieto <jaime.p.r023@gmail.com>
2024-04-26 10:13:14 +0200

    terminado

--- src/main/webapp/js.js
+++ src/main/webapp/js.js
@@ -3,6 +3,7 @@
  */
 
 const BDD = [["Pedro", 8], ["Juan", 5], ["Manuel", 10], ["Alberto", 4], ["Igor", 2],];
+var BDDMOD = [];
 const ruta = document.getElementById("BDD");
 
 //Se escribe completo por pantalla
@@ -15,54 +16,57 @@ ruta.innerHTML = resultado;
 //Los escribe todos
 function todos() {
 	resultado = "";
+	BDDMOD = [];
 	for (let i = 0; i < BDD.length; i++) {
 		resultado += BDD[i] + "<br>";
+		BDDMOD.push(BDD[i]);
 	}
 	ruta.innerHTML = resultado;
-	return resultado;
-
 }
 
 //Solo para ver los suspensos
 function suspensos() {
 	resultado = "";
+	BDDMOD = [];
 	ruta.innerHTML = "";
 	for (let i = 0; i < BDD.length; i++) {
 		if (BDD[i][1] <= 4) {
 			resultado += BDD[i] + "<br>";
+			BDDMOD.push(BDD[i]);
 		}
 	}
 	ruta.innerHTML = resultado;
-	return resultado;
 }
+
 //Solo para ver los aprobados
 function aprobados() {
 	resultado = "";
+	BDDMOD = [];
 	ruta.innerHTML = "";
 	for (let i = 0; i < BDD.length; i++) {
 		if (BDD[i][1] >= 5) {
 			resultado += BDD[i] + "<br>";
+			BDDMOD.push(BDD[i]);
 		}
 	}
 	ruta.innerHTML = resultado;
-	return resultado;
 
 }
 
 //Se ordena por notas
 function porNota() {
 	resultado = "";
-	for (let i = 0; i < BDD.length - 1; i++) {
-		for (let j = 0; j < BDD.length - 1 - i; j++) {
-			if (BDD[j][1] > BDD[j + 1][1]) {
-				let aux = BDD[j];
-				BDD[j] = BDD[j + 1];
-				BDD[j + 1] = aux;
+	for (let i = 0; i < BDDMOD.length - 1; i++) {
+		for (let j = 0; j < BDDMOD.length - 1 - i; j++) {
+			if (BDDMOD[j][1] > BDDMOD[j + 1][1]) {
+				let aux = BDDMOD[j];
+				BDDMOD[j] = BDDMOD[j + 1];
+				BDDMOD[j + 1] = aux;
 			}
 		}
 	}
-	for (let i = 0; i < BDD.length; i++) {
-		resultado += BDD[i] + "<br>";
+	for (let i = 0; i < BDDMOD.length; i++) {
+		resultado += BDDMOD[i] + "<br>";
 	}
 	ruta.innerHTML = resultado;
 	//Con el sort
@@ -81,17 +85,17 @@ function porNota() {
 //Se ordena por nombre
 function porNombre() {
 	resultado = "";
-	for (let i = 0; i < BDD.length - 1; i++) {
-		for (let j = 0; j < BDD.length - 1 - i; j++) {
-			if (BDD[j][0] > BDD[j + 1][0]) {
-				let aux = BDD[j];
-				BDD[j] = BDD[j + 1];
-				BDD[j + 1] = aux;
+	for (let i = 0; i < BDDMOD.length - 1; i++) {
+		for (let j = 0; j < BDDMOD.length - 1 - i; j++) {
+			if (BDDMOD[j][0] > BDDMOD[j + 1][0]) {
+				let aux = BDDMOD[j];
+				BDDMOD[j] = BDDMOD[j + 1];
+				BDDMOD[j + 1] = aux;
 			}
 		}
 	}
-	for (let i = 0; i < BDD.length; i++) {
-		resultado += BDD[i] + "<br>";
+	for (let i = 0; i < BDDMOD.length; i++) {
+		resultado += BDDMOD[i] + "<br>";
 	}
 	ruta.innerHTML = resultado;
 
